601b770dcfe71bfd5a4725ab611a7671f056e656
yoshin <yoshin@student.42gyeongsan.kr>
2024-10-07 16:16:48 +0900

    fix:ft_atoi.c, libft.h
    fix errors

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,19 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 18:46:36 by yoshin            #+#    #+#             */
-/*   Updated: 2024/10/06 01:50:37 by yoshin           ###   ########.fr       */
+/*   Updated: 2024/10/07 16:14:36 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_isspace(int c)
+{
+	if (c == ' ' || c == '\f' || c == '\n'
+		|| c == '\v' || c == '\t' || c == '\r')
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(const char *nptr)
 {
@@ -32,12 +40,4 @@ int	ft_atoi(const char *nptr)
 	if (flag)
 		return ((result) * (-1));
 	return (result);
-}
-
-int	ft_isspace(int c)
-{
-	if (c == ' ' || c == '\f' || c == '\n'
-		|| c == '\v' || c == '\t' || c == '\r')
-		return (1);
-	return (0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:25:16 by yoshin            #+#    #+#             */
-/*   Updated: 2024/10/03 11:19:59 by yoshin           ###   ########.fr       */
+/*   Updated: 2024/10/07 16:15:32 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*memmove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 /* ------ */
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
@@ -49,8 +49,8 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 
 /*  Todo  */
-void	*calloc(size_t nmemb, size_t size);
-char	*strdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 /* ------ */
 
 /* Additionals */
