0f1346cd8f9daa7c256324ec767bedce000891be
yoshin <yongdevshop@gmail.com>
2024-10-07 21:39:48 +0900

    feat: ft_memchr
    SYNOPSIS
      #include <string.h>
    
      void *memchr(const void *s, int c, size_t n);
    
    DESCRIPTION
    
      The memchr() function scans the initial n bytes of the memory
      area pointed to by s for the first instance of c. Both c and
      the bytes of the memory area pointed to by s are interpreted
      as unsigned char.
    
    RETURN VALUE
    
      The memchr() and memrchr() functions return a pointer to the
      matching byte or NULL if the character does not occur in the
      given memory area.

---
+++ ft_memchr.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 21:22:35 by yoshin            #+#    #+#             */
+/*   Updated: 2024/10/07 21:34:03 by yoshin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	while (n--)
+		if (*(unsigned char *)s++ == (unsigned char)c)
+			return ((void *)--s);
+	return (NULL);
+}
