15b5ce2134b7b0e9bd5913fc69d669ae3b740ef7
yoshin <yoshin@student.42gyeongsan.kr>
2024-10-08 09:16:09 +0900

    fix: libft.h
    norminette error fixed

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yoshin <yoshin@student.42gyeongsan.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:25:16 by yoshin            #+#    #+#             */
-/*   Updated: 2024/10/07 16:27:46 by yoshin           ###   ########.fr       */
+/*   Updated: 2024/10/08 09:15:46 by yoshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_isspace(int c);
 
 size_t	ft_strlen(const char *s);
 
@@ -27,6 +28,7 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 /* ------ */
@@ -69,7 +71,7 @@ void	ft_putnbr_fd(int n, int fd);
 /* ----------- */
 
 /* Bonus Part */
-typedef struct	s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
